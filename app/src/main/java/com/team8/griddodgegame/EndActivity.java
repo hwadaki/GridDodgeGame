2548d7cde3c0d0f8c8085a331c99e0d6cbce62f3
padagi <padagi5678@naver.com>
2023-11-25 20:45:17 +0900

    개발중

--- app/src/main/java/com/team8/griddodgegame/EndActivity.java
+++ app/src/main/java/com/team8/griddodgegame/EndActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EndActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
+
+
     }
 }
--- app/src/main/java/com/team8/griddodgegame/GameActivity.java
+++ app/src/main/java/com/team8/griddodgegame/GameActivity.java
@@ -2,17 +2,58 @@ package com.team8.griddodgegame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class GameActivity extends AppCompatActivity {
+    int[][] GridTotal = new int[8][8];
+
+    int[][] GridUp = new int[8][8]; //위로 1
+    int[][] GridRight = new int[8][8]; //오른쪽으로 2
+    int[][] GridDown = new int[8][8]; //밑으로 3
+    int[][] GridLeft = new int[8][8]; //왼쪽으로 4
+
+
+    public int[][] gridPushing(int direction, int[][] gridArray){ //그리드 한칸 밀기
+        Random rand = new Random();
+
+        switch (direction){
+            case 1: //위로
+                for(int i=0;i<gridArray.length;i++){
+                    for(int j=0;j<gridArray[0].length;j++){
+                        gridArray[i][j] = gridArray[i+1][j];
+                    }
+                }
+                int randomIndex = rand.nextInt(gridArray[0].length);
+
+                break;
+        }
+
+        return gridArray;
+    };
+
+    public boolean gridChange(int x, int y){ //그리드 병합 후, 닿았는 지 안 닿았는 지 여부 반환
+
+
+        return false;
+    };
 
     ImageButton upKey, leftKey, rightKey, downKey;
     TextView scoreText;
 
     int heartNum;
+
+    int score = 0;
+    int enemyDensity;
+    int playerX, playerY;
+
+    boolean damagedTF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +66,66 @@ public class GameActivity extends AppCompatActivity {
         rightKey = findViewById(R.id.rightKey);
         scoreText = findViewById(R.id.scoreText);
 
+        Arrays.fill(GridUp,0);
+        Arrays.fill(GridDown,0);
+        Arrays.fill(GridLeft,0);
+        Arrays.fill(GridRight,0);
+        Arrays.fill(GridTotal,0);
+
         Intent intent = getIntent();
 
         Integer gameLevel = intent.getIntExtra("gameLevel",0);
+        switch(gameLevel){
+            case 1:
+                heartNum = 1;
+                enemyDensity = 3;
+                break;
 
-        int[][] GridTotal = new int[10][10];
+            case 2:
+                heartNum = 2;
+                enemyDensity = 2;
+                break;
+
+            case 3:
+                heartNum = 3;
+                enemyDensity = 1;
+                break;
+        }
+
+        playerX = 4; playerY = 4;
+
+        upKey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(playerY>=1){ //이동
+                    playerY--;
+                }
+
+                damagedTF = gridChange(playerX, playerY); //폭탄 위치 갱신 후 닿았는 지 여부 확인
+
+                //+xml 그리드 갱신
+
+                if(damagedTF){ //닿았을 때
+                    if(heartNum > 1){
+                        heartNum--;
+                        //+xml 하트 갯수 갱신
+                        //+닿는 소리 재생
+                    }
+                    else{
+                        //+게임오버 소리 재생
+
+                        Intent intent = new Intent(getApplicationContext(), EndActivity.class);
+                        intent.putExtra("score",score);
+
+                        startActivity(intent);
+                    }
+                } else{ //안 닿았을 때
+
+                    //+발걸음 소리 재생
+                }
+
+            }
+        });
+
     }
 }
