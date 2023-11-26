13d678628d1abbe8e15155801a6ab8aea5a2d9eb
padagi <padagi5678@naver.com>
2023-11-26 12:02:35 +0900

    main, EndActivity 병합 완료

--- app/src/main/java/com/team8/griddodgegame/EndActivity.java
+++ app/src/main/java/com/team8/griddodgegame/EndActivity.java
@@ -1,14 +1,39 @@
 package com.team8.griddodgegame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EndActivity extends AppCompatActivity {
 
+    ImageButton restartBtn;
+    TextView scoreText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_end);
+
+        restartBtn = findViewById(R.id.restartBtn);
+        scoreText = findViewById(R.id.scoreText);
+
+
+        Intent intent = getIntent();
+
+        restartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent endintent = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(endintent);
+            }
+        });
+
+
     }
 }
